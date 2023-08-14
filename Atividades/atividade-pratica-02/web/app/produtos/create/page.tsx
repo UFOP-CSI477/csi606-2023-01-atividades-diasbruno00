43abf8d9c05735cd5b7e873dc2a75014a03f5f84
bruno dias <diasbruno00@gmail.com>
2023-08-14 14:08:05 -0300

    cadastro de produtos implementado

---
+++ Atividades/atividade-pratica-02/web/app/produtos/create/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import axios from 'axios';
+import { ChangeEvent, FormEvent, useState } from "react"
+
+
+
+
+export default function CreateProdutos(){
+    
+    const [descrisao, setDescrisao] = useState('')
+    const [valorUnitario, setValorUnitario] = useState('')
+
+    const handleSumit =  async (event: FormEvent) => {
+         event.preventDefault()
+
+         const data  ={
+            descrisao,
+            valorUnitario
+         }
+         try {
+
+            const response = await axios.post('http://localhost:5555/produto', data)
+            const produto = await response.data
+            alert('sucesso')
+        
+        } catch (error) {
+            alert(`Erro na inclusão ${descrisao}`)
+        }
+    }
+
+    return(
+        <div className="container col-md-9 ">
+        <h1>Cadastro de produtos: {descrisao}</h1>
+
+        <form onSubmit={handleSumit} >
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="htmlForm-label">Descrisao</label>
+                <input type="text"value={descrisao} className="form-control"  placeholder='Informe a descrisao do produto' onChange={(event: ChangeEvent<HTMLInputElement>) => { setDescrisao(event.target.value) }} required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="htmlForm-label">Valor Unitario</label>
+                <input type="number" value={valorUnitario} className="form-control"  placeholder='Informe o valor unitariow do produto'onChange={(event: ChangeEvent<HTMLInputElement>) => { setValorUnitario(event.target.value) }} required />
+            </div>
+
+            <button type="submit" className="btn btn-primary">Cadastrar</button>
+        </form>
+       </div>
+    )
+}
